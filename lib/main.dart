9951b35d4caa253694a60c0b7b9f01299e56d2dd
Ashutosh Singh <ashutosh@Ashutoshs-MacBook-Pro.local>
2025-04-03 00:20:42 +0530

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'main.g.dart';
 
 void main() {
   runApp(
@@ -10,24 +7,18 @@ void main() {
   );
 }
 
-@riverpod
-String counter(Ref ref){
-  return "Ashutosh Singh Sikarwaar !!";
-}
-
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    String name = ref.watch(counterProvider);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Riverpod Example"),),
         body: Center(
           child: Column(
             children: [
-              Text("Hello World Statefulss " + name),
+              Text("Hello World Statefulss simple"),
             ],
           ),
         ),
